6a088884352965cff6a49718e36d9af665e2a3b6
oscar <oscarvac74@gmail.com>
2024-02-06 16:08:42 -0600

    asyncawait

--- KeyDevConcepts/array.js
+++ KeyDevConcepts/array.js
@@ -1,6 +1,24 @@
-const myArray = [1,2,3,4];
+// const myArray = [1,2,3,4];
 
-myArray.includes()
-
-myArray.map()
+// myArray.map(e => e + 1)
+// [2,3,4,5]
 // Creates new Arrays
+
+// .filter()
+//New array
+// const myArray = [1,3,5,7,9]
+// myArray.filter(el => el> 4)
+// [5,7,9]
+
+// myArray.includes()
+// const myArray =[1,2,3,4,5]
+// myArray.includes(3)
+// true
+// myArray.includes(9) => false
+
+// const newArray = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// Index includes if 2 is included staring at index 3
+// myArray.includes(2,3) => false
+
+//! Primitive Types
+//  Strings,booleans,null,undef,number,symbol
--- KeyDevConcepts/async.js
+++ KeyDevConcepts/async.js
@@ -1,1 +1,14 @@
-const myPromise
+const myPromise = new Promise((resolve, reject) => {
+  if (true) {
+    setTimeout(() => {
+      resolve("I have succeeded");
+    }, 1000);
+  } else {
+    reject("I have failed");
+  }
+});
+//  Reject is to unsure error handling
+myPromise
+  .then((value) => console.log(value))
+  .then((newValue) => console.log(newValue))
+  .catch((rejectValue) => console.log(rejectValue));
