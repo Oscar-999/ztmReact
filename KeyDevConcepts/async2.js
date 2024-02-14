4d60350311faad50cd192c7afc2dcc429b99543b
oscar <oscarvac74@gmail.com>
2024-02-13 19:22:44 -0600

    async

--- KeyDevConcepts/async2.js
+++ KeyDevConcepts/async2.js
@@ -1,11 +1,25 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => {
-        const firstUser = users[0];
-        console.log(firstUser)
-        return fetch(
-            'https://jsonplaceholder.typicode.com/posts?userId=' + firstUser.id
-        )
-    })
-    .then(response => response.json())
-    .then(posts => console.log(posts))
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    const firstUser = users[0];
+    console.log(firstUser);
+    return fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=" + firstUser.id
+    );
+  })
+  .then((response) => response.json())
+  .then((posts) => console.log(posts));
+
+const myAsyncFunction = async () => {
+  const userResponse = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  const users = await userResponse.json();
+  const secondUser = users[1];
+  console.log(secondUser);
+  const postResponse = fetch(
+    "https://jsonplaceholder.typicode.com/posts?userId=" + secondUser.id
+  );
+  const posts = (await postResponse).json();
+  console.log(posts)
+};
