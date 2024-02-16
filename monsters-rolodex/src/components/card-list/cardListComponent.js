1b691617b0c08d7bf9a55159653ebbbe3e8b9f5d
oscar <oscarvac74@gmail.com>
2024-02-16 11:43:04 -0600

    shortcuts

---
+++ monsters-rolodex/src/components/card-list/cardListComponent.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+    render ( ) {
+        return (
+            <div>Hi this is the CardList Component</div>
+        )
+    }
+}
+
+export default CardList
