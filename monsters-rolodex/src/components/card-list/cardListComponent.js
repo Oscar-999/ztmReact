f784e95d5653460e264becde9bfb347fba6d6e32
oscar <oscarvac74@gmail.com>
2024-02-16 19:56:13 -0600

    monsters rolodex

--- monsters-rolodex/src/components/card-list/cardListComponent.js
+++ monsters-rolodex/src/components/card-list/cardListComponent.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import './cardListStyles.css'
-import Card from '../card/cardComponent';
+import React, { Component } from "react";
+import "./cardListStyles.css";
+import Card from "../card/cardComponent";
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <div className='card-list'>
+      <div className="card-list">
         {monsters.map((monster) => {
-            const {name, id , email} = monster
-            return (
-          <Card monster={monster}/>
-        )})}
+          return <Card monster={monster} />;
+        })}
       </div>
     );
   }
