10532629f70ba7f259f81b21cc23a5ed306e9b0d
oscar <oscarvac74@gmail.com>
2024-03-01 20:10:59 -0600

    push

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,113 +1,46 @@
-import CardList from "./components/card-list/cardListComponent";
-import SearchBox from "./components/search-box/searchBoxComponent";
-import "./App.css";
-import { useState, useEffect } from "react";
-import { toBeInTheDOM } from "@testing-library/jest-dom/dist/matchers";
+import { useState, useEffect } from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
 
 const App = () => {
-  const [searchField, setSearchField] = useState("");
+  const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
-  const [title,setTitle] = useState('')
-  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-  console.log("render");
-
+  console.log('rendered');
+//s
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => setMonsters(users));
   }, []);
 
   useEffect(() => {
-    const newfilteredMonsters = monsters.filter((monster) => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
-    setFilteredMonsters(newfilteredMonsters);
-  }, [monsters,searchField]);
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
 
-  const onTitleChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString)
-  }
   return (
-    <div className="App">
-      <h1 className="app-title">{title}</h1>
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
       <SearchBox
+        className='monsters-search-box'
         onChangeHandler={onSearchChange}
-        placeholder="search monsters"
-        className="search-box"
-      />
-      <br/>
-      <SearchBox
-        onChangeHandler={onTitleChange}
-        placeholder="set title"
-        className="title-search-box"
+        placeholder='search monsters'
       />
       <CardList monsters={filteredMonsters} />
     </div>
   );
 };
-
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//     console.log("constructor");
-//   }
-
-//   componentDidMount() {
-//     console.log("compenentdidmount");
-//   fetch("https://jsonplaceholder.typicode.com/users")
-//     .then((response) => response.json())
-//     .then((users) =>
-//       this.setState(
-//         () => {
-//           return { monsters: users };
-//         },
-//         () => {
-//           console.log(this.state);
-//         }
-//       )
-//     );
-// }
-
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLocaleLowerCase();
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   };
-
-//   render() {
-//     console.log("render");
-
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox
-//           onChangeHandler={onSearchChange}
-//           placeholder="search monsters"
-//           className="search-box"
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
