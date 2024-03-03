17d4aaa39094d9e62924880dcedc00cb5a509159
oscar <oscarvac74@gmail.com>
2024-03-03 15:29:44 -0600

    gatsby3

---
+++ gatsby-blog/src/pages/page-3.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE THIRD </h1>
+  </Layout>
+)
+
+
+
+export default ThirdPage
